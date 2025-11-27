cf7315fb03b9d7bf76dc1268dad9b34e8264d254
n2thghm <git.antibody876@passinbox.com>
2025-11-27 11:07:44 +0100

    feat: page random album

---
+++ application/baseHTML.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $metadata['title'] ?></title>
+</head>
+<body>
+    <?php require $component['header'] ?>
+
+    <?php require $component['footer'] ?>
+</body>
+</html>
