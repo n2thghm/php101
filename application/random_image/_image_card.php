1086883cc7073fc0d376b14156f4fdcbceac98d0
Nithachan <nithachan.thuraisingham@tetris-assurance.com>
2025-11-28 20:17:02 +0100

    refacto: page album

--- application/random_image/_image_card.php
+++ application/random_image/_image_card.php
@@ -1,31 +1,31 @@
 <div class="col">
     <div class="card shadow-sm">
-    <img src="<?= $image['src'] ?>">
-    <div class="card-body">
-        <p class="card-text">
-        This is a wider card with supporting text below as a natural
-        lead-in to additional content. This content is a little bit
-        longer.
-        </p>
-        <div
-        class="d-flex justify-content-between align-items-center"
-        >
-        <div class="btn-group">
-            <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
+        <img src="<?= $image['src'] ?>">
+        <div class="card-body">
+            <p class="card-text">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit
+            longer.
+            </p>
+            <div
+            class="d-flex justify-content-between align-items-center"
             >
-            View
-            </button>
-            <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            >
-            Edit
-            </button>
+            <div class="btn-group">
+                <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary"
+                >
+                View
+                </button>
+                <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary"
+                >
+                Edit
+                </button>
+            </div>
+            <small class="text-body-secondary">9 mins</small>
+            </div>
         </div>
-        <small class="text-body-secondary">9 mins</small>
-        </div>
-    </div>
     </div>
 </div>
---
+++ application/random_image/randomImage_old.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta
+      name="author"
+      content="Mark Otto, Jacob Thornton, and Bootstrap contributors"
+    />
+    <meta name="generator" content="Astro v5.13.2" />
+    <title>Album example · Bootstrap v5.3</title>
+    <link
+      rel="canonical"
+      href="https://getbootstrap.com/docs/5.3/examples/album/"
+    />
+    
+    <meta name="theme-color" content="#712cf9" />
+
+  </head>
+  <body>
+
+
+
+
+    <script
+      src="../assets/dist/js/bootstrap.bundle.min.js"
+      class="astro-vvvwv3sm"
+    ></script>
+  </body>
+</html>
